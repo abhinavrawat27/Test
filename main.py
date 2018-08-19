5e343023ddafa6bf19915db5f1b86cf66a21c5ae
Abhinav Rawat <abhinav@thingtrax.com>
2018-08-19 15:51:15 +0530

    update 5

--- main.py
+++ main.py
@@ -1,36 +1,9 @@
-# from flask import Flask, request
-# from flask_pymongo import PyMongo
-
-
-# def main():
-#    app = Flask(__name__)
-
-#    app.config['MONGO_DBNAME'] = 'connect_to_db'
-#    app.config['MONGO_URI'] = 'mongodb://john:qwerty123@ds125272.mlab.com:25272/connect_to_db'
-
-    # app.config['MONGO_DBNAME'] = 'RipeTestDb'
-    # app.config['MONGO_URI'] = 'mongodb://root:zV1Q1aDEZI1s@40.113.223.108:27017/RipeTestDb'
-
-#    mongo = PyMongo(app)
-
-#    @app.route('/')
-#    def add():
-#        return 'Added user'
-
-#    if __name__ == '__main__':
-#        app.run(debug=True)
-
-
-# main()
-
 from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
-    return 'Hello, Wo3rld!'
-
+  return 'Hey its Python Flask application!'
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
